5d8677bc581e9cf15880ca2e89c1a8f3765681a1
Leônidas Amorim <leonidasamorim@gmail.com>
2026-05-12 17:15:52 -0300

    add quadriciulo in category motocycle

--- categoria-moto-v2.php
+++ categoria-moto-v2.php
@@ -3,7 +3,8 @@ $categorias = [
     'Novato'                                         => 'NOVATO',
     'Intermediário'                                  => 'INTERMEDIÁRIO',
     'Over45'                                         => 'OVER45',
-    'Graduado'                                       => 'GRADUADO'
+    'Graduado'                                       => 'GRADUADO',
+    'Quadriculo'                                     => 'QUADRICULO'
 ];
 foreach ($categorias as $val => $label) {
     echo '<option value="' . htmlspecialchars($val) . '">' . htmlspecialchars($label) . "</option>\n";
